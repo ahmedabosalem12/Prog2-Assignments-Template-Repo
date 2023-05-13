3fcaa418cebb3ac71d727371fd417259b52a6d24
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2023-05-13 23:45:26 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,29 @@
+//ahmed salem
+//20216002
+package BankSystem;
+
+public class Employee extends Human {
+    private double salary;
+    private String department;
+
+    public Employee(String name, int age, String gender, double salary, String department) {
+        super(name, age, gender);
+        this.salary = salary;
+        this.department = department;
+    }
+
+    @Override
+    protected void printData() {
+        super.printData();
+        System.out.println("Salary: " + salary);
+        System.out.println("Department: " + department);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
